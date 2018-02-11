5570e2a23b3ac754053d2130494eebd429a32567
Daniil Borovkov <daniilborowkow@yandex.ru>
2018-02-11 11:27:05 +0600

    Initial commit

---
+++ src/components/quiz.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <Layout>
+      <Quiz v-if="loaded"/>
+    </Layout>
+    <b-loading :active.sync="isLoading" :canCancel="true"></b-loading>
+  </div>
+</template>
+
+<script>
+  import Layout from '@/components/Layout';
+  import Quiz from '@/components/Quiz';
+  import { mapState } from 'vuex';
+
+  export default {
+    beforeMount() {
+      this.$store.dispatch('quiz/get', this.$route.params.id).then(() => {
+        this.isLoading = false;
+      });
+    },
+    data() {
+      return {
+        isLoading: true,
+      };
+    },
+    computed: {
+      ...mapState('quiz', ['loaded']),
+    },
+    components: {
+      Layout, Quiz,
+    },
+    name: 'app',
+  };
+</script>
+
+<style src="@/app.less" lang="less"></style>
